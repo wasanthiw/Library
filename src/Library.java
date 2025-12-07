afb7a50f8bfff9936eed68cf4959c2cf6a1d783a
cristopherhsv <cristopherhsv04@gmail.com>
2025-12-06 22:34:41 -0800

    Update Library.java
    add book method

--- src/Library.java
+++ src/Library.java
@@ -26,6 +26,17 @@ public class Library {
      * Displays all books in the library.
      * If the library is empty, prints a message indicating no books are available.
      */
+
+    /**
+     * Adds a book to the library collection.
+     *
+     * @param book The Book object to add
+     */
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added: " + book.getTitle());
+    }
+    
     public void displayBooks() {
         if (books.isEmpty()) {
             System.out.println("No books in the library");
